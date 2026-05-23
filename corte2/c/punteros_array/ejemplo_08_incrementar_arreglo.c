4b7350846ea17335eea7a5622534d2ab1773ddf7
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-05-23 14:02:36 -0500

    Create ejemplo_08_incrementar_arreglo.c

---
+++ corte2/c/punteros_array/ejemplo_08_incrementar_arreglo.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void incrementarTodos(int *datos, int n) {
+    /* i sera el indice usado para recorrer el arreglo. */
+    int i;
+
+    /* Verificamos que el puntero sea valido. */
+    if (datos == NULL) {
+        return;
+    }
+
+    /* Recorremos el arreglo y aumentamos cada elemento en una unidad. */
+    for (i = 0; i < n; i++) {
+        datos[i] = datos[i] + 1;
+    }
+}
+
+void mostrarArreglo(int *datos, int n) {
+    /* i sera el indice usado para recorrer el arreglo. */
+    int i;
+
+    /* Recorremos el arreglo para mostrar sus elementos. */
+    for (i = 0; i < n; i++) {
+        printf("%d ", datos[i]);
+    }
+
+    printf("\n");
+}
+
+int main() {
+    /* Declaramos un arreglo de cinco enteros. */
+    int datos[5] = {8, 3, 15, 6, 10};
+
+    /* Mostramos el arreglo antes del cambio. */
+    printf("Antes: ");
+    mostrarArreglo(datos, 5);
+
+    /* Modificamos el arreglo original desde una funcion. */
+    incrementarTodos(datos, 5);
+
+    /* Mostramos el arreglo despues del cambio. */
+    printf("Despues: ");
+    mostrarArreglo(datos, 5);
+
+    return 0;
+}
