981ad1311a6a85dd0491df75ed9671d376150345
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-05-23 12:29:45 -0500

    Create ejercicio7.cpp

---
+++ corte2/c++/figurasloop/ejercicio7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int fila, columna;
+    int n = 5;
+    int ancho = 2 * n - 1;
+
+    for (fila = 1; fila <= ancho; fila++) {
+        int distancia = (fila <= n) ? (n - fila) : (fila - n);
+
+        for (columna = 1; columna <= ancho; columna++) {
+            if (columna == distancia + 1 || columna == ancho - distancia) {
+                cout << "$";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
