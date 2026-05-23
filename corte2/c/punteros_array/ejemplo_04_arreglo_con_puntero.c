c49c52ab6a908b561284dd35b8dbcd7e078c53ce
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-05-23 13:20:55 -0500

    Update ejemplo_04_arreglo_con_puntero.c

--- corte2/c/punteros_array/ejemplo_04_arreglo_con_puntero.c
+++ corte2/c/punteros_array/ejemplo_04_arreglo_con_puntero.c
@@ -1,19 +1,19 @@
- #include <stdio.h>
+#include <stdio.h>
 
 int main() {
-    /* Declaramos un entero de cinco enteros. */
-    int datos[5] = {8, 3, 15, 6, 10}
+    /* Declaramos un arreglo de cinco enteros. */
+    int datos[5] = {8, 3, 15, 6, 10};
 
-    /* p apunta al primer elemento del arreglo.*/
+    /* p apunta al primer elemento del arreglo. */
     int *p = datos;
 
-    /* i sera l indice usado para recorrer el arreglo. */
+    /* i será el índice usado para recorrer el arreglo. */
     int i;
 
-    /* Recorremos el arreglo usando aritmetica de punteros. */
+    /* Recorremos el arreglo usando aritmética de punteros. */
     for (i = 0; i < 5; i++) {
-        /* *(p + i) accede al elemnto ubicado i posiciones despues de p. */
-        printf("Elemento %d = %d\n", i, *(p + i);
+        /* *(p + i) accede al elemento ubicado i posiciones después de p. */
+        printf("Elemento %d = %d\n", i, *(p + i));
     }
 
     return 0;
