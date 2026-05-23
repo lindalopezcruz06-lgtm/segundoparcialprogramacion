f1e5de03f5514b077a70e6ff35349e0df8450ff8
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-05-23 13:14:33 -0500

    Create ejemplo_02_recorrido_arreglo.c

---
+++ corte2/c/punteros_array/ejemplo_02_recorrido_arreglo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    /* Declaramos un arreglo de cinco enteros. */
+    int datos[5] = {8, 3, 15, 6, 10};
+
+    /* i sera el indice usado para recorrer el arreglo. */
+    int i;
+
+    /* Recorremos los indices validos: 0, 1, 2, 3 y 4. */
+    for (i = 0; i < 5; i++) {
+        /* En cada vuelta se muestra el elemento datos[i]. */
+        printf("datos[%d] = %d\n", i, datos[i]);
+    }
+
+    return 0;
+}
