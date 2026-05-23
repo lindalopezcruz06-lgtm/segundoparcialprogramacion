4a3be564437ccdce0e6d3082cc081f27206c444a
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-05-23 11:36:14 -0500

    Create ejemplo_02_operador_direccion.cpp

---
+++ corte2/c++/punteros/ejemplo_02_operador_direccion.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int x = 10;
+
+    cout << "Valor de x = " << x << endl;
+    cout << "Direccion de x = " << &x << endl;
+
+    return 0;
+}
