0aee455380b442494daad0fe64c409fd99148ff9
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:35:11 -0500

    Update ejercicio1.cpp

--- corte2/c++/elementosnumericos/ejercicio1.cpp
+++ corte2/c++/elementosnumericos/ejercicio1.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double celsius, fahrenheit;
+    cout << "Ingrese grados Celsius: ";
+    cin >> celsius;
+    
+    fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+    cout << "Equivale a: " << fahrenheit << " F" << endl;
+    return 0;
+}
 
