5ed4da66f7808db0997559e82b2cb97e185b4a84
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:47:16 -0500

    Update programa5.cpp

--- corte2/c++/elementosnumericos/programa5.cpp
+++ corte2/c++/elementosnumericos/programa5.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double nota1, nota2, nota3, promedio;
+    
+    // Lectura secuencial de las tres calificaciones
+    cout << "Ingrese la primera nota: ";
+    cin >> nota1;
+    cout << "Ingrese la segunda nota: ";
+    cin >> nota2;
+    cout << "Ingrese la tercera nota: ";
+    cin >> nota3;
+    
+    // Calcula la media dividiendo de forma exacta entre 3.0
+    promedio = (nota1 + nota2 + nota3) / 3.0;
+    
+    // Muestra el promedio final obtenido en pantalla
+    cout << "El promedio final es: " << promedio << endl;
+    
+    return 0;
+}
 
