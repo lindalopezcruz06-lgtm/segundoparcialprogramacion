ef57b45707779e670acbc0261a65b060430a3f18
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:33:47 -0500

    Update conversion_implicita.cpp

--- corte2/c++/elementosnumericos/conversion_implicita.cpp
+++ corte2/c++/elementosnumericos/conversion_implicita.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int entero = 5;
+    double decimal = entero; // Conversion implicita automática
+    
+    cout << "Valor entero: " << entero << endl;
+    cout << "Valor convertido a double: " << decimal << endl;
+    return 0;
+}
 
