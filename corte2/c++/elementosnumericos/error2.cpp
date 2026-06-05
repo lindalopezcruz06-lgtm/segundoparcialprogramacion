c38a30e87a6c8f9001c784f499738b8f40e9a649
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:36:27 -0500

    Update error2.cpp

--- corte2/c++/elementosnumericos/error2.cpp
+++ corte2/c++/elementosnumericos/error2.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numerador = 10;
+    int denominador = 0;
+    
+    if (denominador == 0) {
+        cout << "Error: No se puede dividir entre cero de forma segura." << endl;
+    } else {
+        cout << "Resultado: " << (numerador / denominador) << endl;
+    }
+    return 0;
+}
 
