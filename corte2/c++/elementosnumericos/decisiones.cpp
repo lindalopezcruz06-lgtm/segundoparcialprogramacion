cfd46ee89d5e44a4254ab28cc7912c8f4d17415b
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:34:02 -0500

    Update decisiones.cpp

--- corte2/c++/elementosnumericos/decisiones.cpp
+++ corte2/c++/elementosnumericos/decisiones.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int nota = 75;
+    
+    if (nota >= 60) {
+        cout << "Aprobado" << endl;
+    } else {
+        cout << "Reprobado" << endl;
+    }
+    return 0;
+}
 
