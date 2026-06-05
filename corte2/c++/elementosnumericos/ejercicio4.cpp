2224d1e09191ac633cef80f5fa032d0cc3613cdc
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:35:54 -0500

    Update ejercicio4.cpp

--- corte2/c++/elementosnumericos/ejercicio4.cpp
+++ corte2/c++/elementosnumericos/ejercicio4.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 10, aux;
+    cout << "Antes: a = " << a << ", b = " << b << endl;
+    
+    aux = a;
+    a = b;
+    b = aux;
+    
+    cout << "Despues: a = " << a << ", b = " << b << endl;
+    return 0;
+}
 
