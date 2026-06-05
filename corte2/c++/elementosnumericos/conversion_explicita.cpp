50fb83b0785b53d7601febbb42141aadd6adbe08
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:33:34 -0500

    Update conversion_explicita.cpp

--- corte2/c++/elementosnumericos/conversion_explicita.cpp
+++ corte2/c++/elementosnumericos/conversion_explicita.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double pi = 3.14159;
+    int pi_entero = static_cast<int>(pi); // Conversion explicita
+    
+    cout << "Double original: " << pi << endl;
+    cout << "Entero truncado: " << pi_entero << endl;
+    return 0;
+}
 
