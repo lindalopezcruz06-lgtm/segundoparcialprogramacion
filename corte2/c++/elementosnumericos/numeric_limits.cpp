9e6fcfcef228cbddef663ce7e5fc276e467fa3a4
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:32:00 -0500

    Update numeric_limits.cpp

--- corte2/c++/elementosnumericos/numeric_limits.cpp
+++ corte2/c++/elementosnumericos/numeric_limits.cpp
@@ -1,1 +1,14 @@
+#include <iostream> // Permite imprimir en consola.
+#include <limits>   // Incluye numeric_limits.
+#include <cstdint>  // Incluye tipos de tamaño definido.
+using namespace std; // Permite usar cout y endl directamente.
+
+int main() { // Inicia la función principal.
+    cout << "int min: " << numeric_limits<int>::min() << endl; // Menor int.
+    cout << "int max: " << numeric_limits<int>::max() << endl; // Mayor int.
+    cout << "uint8_t max: " << static_cast<int>(numeric_limits<uint8_t>::max()) << endl; // Mayor uint8_t.
+    cout << "int16_t min: " << numeric_limits<int16_t>::min() << endl; // Menor int16_t.
+    cout << "int16_t max: " << numeric_limits<int16_t>::max() << endl; // Mayor int16_t.
+    return 0; // Finaliza el programa correctamente.
+}
 
