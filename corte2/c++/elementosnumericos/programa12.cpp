eedf67c25cd32f92d8051cc0e23d47edaa523ba5
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:48:52 -0500

    Update programa12.cpp

--- corte2/c++/elementosnumericos/programa12.cpp
+++ corte2/c++/elementosnumericos/programa12.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <cstdint> // Necesario para uint8_t
+using namespace std;
+
+int main() {
+    uint8_t numero = 255; // El valor maximo que puede almacenar 8 bits sin signo
+    cout << "Valor inicial: " << static_cast<int>(numero) << endl;
+    
+    numero = numero + 1; // Produce un desbordamiento (overflow) y vuelve al inicio del rango (0)
+    cout << "Valor despues de sumar 1: " << static_cast<int>(numero) << endl;
+    
+    return 0;
+}
 
