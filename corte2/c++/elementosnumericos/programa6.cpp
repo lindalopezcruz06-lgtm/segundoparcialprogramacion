be2e65cb12d1184ffa0431c2b91660987a934c1a
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:47:28 -0500

    Update programa6.cpp

--- corte2/c++/elementosnumericos/programa6.cpp
+++ corte2/c++/elementosnumericos/programa6.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int edad;
+    
+    // Solicita la edad actual del usuario
+    cout << "Ingrese su edad: ";
+    cin >> edad;
+    
+    // Estructura de decision para verificar si es igual o mayor a 18 años
+    if (edad >= 18) {
+        cout << "La persona es MAYOR de edad." << endl;
+    } else {
+        cout << "La persona es MENOR de edad." << endl;
+    }
+    
+    return 0;
+}
 
