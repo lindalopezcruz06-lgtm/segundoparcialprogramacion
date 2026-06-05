c58e28ab89ba2057937325b5beb3dab805c5ac38
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:32:51 -0500

    Update comparacion_tolerancia.cpp

--- corte2/c++/elementosnumericos/comparacion_tolerancia.cpp
+++ corte2/c++/elementosnumericos/comparacion_tolerancia.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double a = 0.1 + 0.2;
+    double b = 0.3;
+    double tolerancia = 1e-9;
+    
+    // Forma correcta usando el valor absoluto de la diferencia
+    if (abs(a - b) < tolerancia) {
+        cout << "Los numeros son iguales dentro de la tolerancia." << endl;
+    } else {
+        cout << "Los numeros son diferentes." << endl;
+    }
+    return 0;
+}
 
