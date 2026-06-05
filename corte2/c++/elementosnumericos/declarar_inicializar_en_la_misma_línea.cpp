1dbb2cfc826921668cd5ff0583e4539a4ef25935
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:34:18 -0500

    Update declarar_inicializar_en_la_misma_línea.cpp

--- corte2/c++/elementosnumericos/declarar_inicializar_en_la_misma_línea.cpp
+++ corte2/c++/elementosnumericos/declarar_inicializar_en_la_misma_línea.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int velocidad_maxima = 120; // Declaracion e inicializacion simultanea
+    cout << "Velocidad: " << velocidad_maxima << " km/h" << endl;
+    return 0;
+}
 
