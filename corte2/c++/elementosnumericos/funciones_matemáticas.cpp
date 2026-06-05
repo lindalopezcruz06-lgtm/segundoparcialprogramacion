ea1fccc9ebb835313256a1f0c74dbf3923e18828
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:36:42 -0500

    Update funciones_matemáticas.cpp

--- corte2/c++/elementosnumericos/funciones_matemáticas.cpp
+++ corte2/c++/elementosnumericos/funciones_matemáticas.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double base = 2.0;
+    double exponente = 3.0;
+    double numero = 16.0;
+    
+    cout << "Potencia (2^3): " << pow(base, exponente) << endl;
+    cout << "Raiz cuadrada de 16: " << sqrt(numero) << endl;
+    return 0;
+}
 
