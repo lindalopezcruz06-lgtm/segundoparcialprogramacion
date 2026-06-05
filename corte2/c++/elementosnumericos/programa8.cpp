e76c3f649091ac5d6e16420cb67bb6ff9d3a484b
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:48:00 -0500

    Update programa8.cpp

--- corte2/c++/elementosnumericos/programa8.cpp
+++ corte2/c++/elementosnumericos/programa8.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <cstdint> // Requerido de forma estandar para compilar uint8_t, uint16_t y uint32_t
+using namespace std;
+
+int main() {
+    // El operador sizeof devuelve la cantidad exacta de bytes ocupados en memoria
+    cout << "Tamaño de int: " << sizeof(int) << " bytes" << endl;
+    cout << "Tamaño de float: " << sizeof(float) << " bytes" << endl;
+    cout << "Tamaño de double: " << sizeof(double) << " bytes" << endl;
+    cout << "Tamaño de char: " << sizeof(char) << " bytes" << endl;
+    cout << "Tamaño de bool: " << sizeof(bool) << " bytes" << endl;
+    cout << "Tamaño de uint8_t: " << sizeof(uint8_t) << " bytes" << endl;
+    cout << "Tamaño de int16_t: " << sizeof(int16_t) << " bytes" << endl;
+    cout << "Tamaño de uint32_t: " << sizeof(uint32_t) << " bytes" << endl;
+    
+    return 0;
+}
 
