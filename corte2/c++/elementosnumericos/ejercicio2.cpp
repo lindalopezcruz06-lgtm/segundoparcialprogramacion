d221beb317be30dc350d1abbfac9b5afb6e695ce
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:35:26 -0500

    Update ejercicio2.cpp

--- corte2/c++/elementosnumericos/ejercicio2.cpp
+++ corte2/c++/elementosnumericos/ejercicio2.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double radio, area;
+    const double PI = 3.14159265;
+    
+    cout << "Ingrese el radio del circulo: ";
+    cin >> radio;
+    
+    area = PI * radio * radio;
+    cout << "El area es: " << area << endl;
+    return 0;
+}
 
