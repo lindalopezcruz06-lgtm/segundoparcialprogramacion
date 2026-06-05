1f7ad25411d76107061719a06450e204452a3c4c
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:46:03 -0500

    Update programa1.cpp

--- corte2/c++/elementosnumericos/programa1.cpp
+++ corte2/c++/elementosnumericos/programa1.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declaracion e inicializacion de tres variables enteras
+    int num1 = 10;
+    int num2 = 20;
+    int num3 = 30;
+    
+    // Calculo de la suma de las tres variables
+    int suma = num1 + num2 + num3;
+    
+    // Muestra el resultado de la suma en la consola
+    cout << "La suma de los tres números es: " << suma << endl;
+    
+    return 0; // Finaliza correctamente el programa
+}
 
