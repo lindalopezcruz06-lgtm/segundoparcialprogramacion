de0fcefc7e72c653cc9d5721e4796fe969318ee0
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:39:55 -0500

    Update valor_guardado.cpp

--- corte2/c++/elementosnumericos/valor_guardado.cpp
+++ corte2/c++/elementosnumericos/valor_guardado.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int conteo_ciclos = 10; // Guarda un valor entero exacto sin punto decimal
+    double factor_escala = 0.005; // Guarda un valor de punto flotante de alta precision
+    char identificador = 'M'; // Guarda un unico caracter delimitado por comillas simples
+    
+    cout << "Entero guardado: " << conteo_ciclos << endl;
+    cout << "Decimal guardado: " << factor_escala << endl;
+    cout << "Caracter guardado: " << identificador << endl;
+    
+    return 0; // Finaliza el script correctamente
+}
 
