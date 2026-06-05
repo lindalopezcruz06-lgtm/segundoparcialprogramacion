a866ef3877775f8ecb1705e73c2d6e4d4abe92a4
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:33:09 -0500

    Update comparaciones_numericas.cpp

--- corte2/c++/elementosnumericos/comparaciones_numericas.cpp
+++ corte2/c++/elementosnumericos/comparaciones_numericas.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = 20;
+    
+    cout << "x < y : " << (x < y) << endl;
+    cout << "x > y : " << (x > y) << endl;
+    cout << "x == y: " << (x == y) << endl;
+    cout << "x != y: " << (x != y) << endl;
+    return 0;
+}
 
