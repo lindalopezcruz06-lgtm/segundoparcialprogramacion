5a7f5f94c2dbbacda84891b83636b847c026de13
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:47:01 -0500

    Update programa4.cpp

--- corte2/c++/elementosnumericos/programa4.cpp
+++ corte2/c++/elementosnumericos/programa4.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double celsius, fahrenheit;
+    
+    // Lee el valor de la temperatura en escala Celsius
+    cout << "Ingrese la temperatura en grados Celsius: ";
+    cin >> celsius;
+    
+    // Realiza la conversion aplicando la equivalencia matematica con punto decimal (9.0 / 5.0)
+    fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+    
+    // Imprime la temperatura equivalente en escala Fahrenheit
+    cout << "La temperatura en grados Fahrenheit es: " << fahrenheit << endl;
+    
+    return 0;
+}
 
