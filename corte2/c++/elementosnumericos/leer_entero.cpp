bb37b220319d1a5b9a2b20a881d0139bbf1c38f4
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:37:14 -0500

    Update leer_entero.cpp

--- corte2/c++/elementosnumericos/leer_entero.cpp
+++ corte2/c++/elementosnumericos/leer_entero.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cantidad;
+    cout << "Ingrese una cantidad entera: ";
+    cin >> cantidad;
+    
+    cout << "Cantidad registrada: " << cantidad << endl;
+    return 0;
+}
 
