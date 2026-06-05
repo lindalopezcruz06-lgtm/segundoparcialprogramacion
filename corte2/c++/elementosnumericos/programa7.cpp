00529bf8c18b383c7120d9ce65c78014ee7b9512
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:47:48 -0500

    Update programa7.cpp

--- corte2/c++/elementosnumericos/programa7.cpp
+++ corte2/c++/elementosnumericos/programa7.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int numero;
+    
+    // Captura el numero entero a evaluar
+    cout << "Ingrese un número entero: ";
+    cin >> numero;
+    
+    // Si el residuo de dividir entre 2 es cero, el numero es par
+    if (numero % 2 == 0) {
+        cout << "El número " << numero << " es PAR." << endl;
+    } else {
+        cout << "El número " << numero << " es IMPAR." << endl;
+    }
+    
+    return 0;
+}
