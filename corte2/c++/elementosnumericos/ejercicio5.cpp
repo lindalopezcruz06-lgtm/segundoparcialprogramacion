de41717047fe1d443424d23689c4aebc09f4711f
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:36:07 -0500

    Update ejercicio5.cpp

--- corte2/c++/elementosnumericos/ejercicio5.cpp
+++ corte2/c++/elementosnumericos/ejercicio5.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double n1, n2, n3, promedio;
+    cout << "Ingrese tres notas separadas por espacios: ";
+    cin >> n1 >> n2 >> n3;
+    
+    promedio = (n1 + n2 + n3) / 3.0;
+    cout << "El promedio final es: " << promedio << endl;
+    return 0;
+}
 
