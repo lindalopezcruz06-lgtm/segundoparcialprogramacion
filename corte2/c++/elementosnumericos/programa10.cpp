1bc6f098012a8b10091a00f900527d006d0c987e
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:48:25 -0500

    Update programa10.cpp

--- corte2/c++/elementosnumericos/programa10.cpp
+++ corte2/c++/elementosnumericos/programa10.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <cstdint> // Necesario para el tipo entero de 16 bits sin signo
+using namespace std;
+
+int main() {
+    // uint16_t ocupa 2 bytes en memoria y maneja un rango sin signo de 0 a 65,535 pulsos
+    uint16_t pulsos_encoder = 45230;
+    
+    // Imprime el valor acumulado de pulsos registrados por el sensor encoder
+    cout << "Pulsos actuales del encoder: " << pulsos_encoder << endl;
+    
+    return 0;
+}
 
