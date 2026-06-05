91b5da1b431fef2491cc49c78cbce2eca010816e
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:34:57 -0500

    Update division_entera_decimal.cpp

--- corte2/c++/elementosnumericos/division_entera_decimal.cpp
+++ corte2/c++/elementosnumericos/division_entera_decimal.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 2;
+    
+    cout << "Division entera (5 / 2): " << (a / b) << endl;
+    cout << "Division decimal (5.0 / 2): " << (static_cast<double>(a) / b) << endl;
+    return 0;
+}
 
