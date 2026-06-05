dad618dd471dfaaf8eb68ef6ba6b7470f08ceed8
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:34:41 -0500

    Update declarar_y_luego_asignar.cpp

--- corte2/c++/elementosnumericos/declarar_y_luego_asignar.cpp
+++ corte2/c++/elementosnumericos/declarar_y_luego_asignar.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int temperatura; // Solo declaracion (contiene basura de memoria)
+    temperatura = 24; // Asignacion posterior
+    
+    cout << "Temperatura registrada: " << temperatura << endl;
+    return 0;
+}
 
