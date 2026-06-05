4af7b0bd5025828638396269dc48c0360719ab15
lindalopezcruz06-lgtm <lindalopezcruz06@gmail.com>
2026-06-05 00:37:01 -0500

    Update leer_decimal.cpp

--- corte2/c++/elementosnumericos/leer_decimal.cpp
+++ corte2/c++/elementosnumericos/leer_decimal.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double medicion;
+    cout << "Ingrese un valor decimal (ej. 12.34): ";
+    cin >> medicion;
+    
+    cout << "El valor leido con exito es: " << medicion << endl;
+    return 0;
+}
 
